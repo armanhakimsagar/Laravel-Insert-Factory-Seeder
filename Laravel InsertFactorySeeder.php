f6fa8d929ec7fb79b67928347493541b29f7fec7
Arman Hakim Sagar <armanhakimsagar@gmail.com>
2017-12-15 23:29:21 +0600

    Update Laravel InsertFactorySeeder.php

--- Laravel InsertFactorySeeder.php
+++ Laravel InsertFactorySeeder.php
@@ -149,3 +149,50 @@ Factory | Seeder ->
    }
    
 -> php artisan db:seed
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+___________________________________
+	
+	
+multiple image upload :
+
+
+
+********* multiple image upload 
+
+
+<form class="form-horizontal" enctype="multipart/form-data" method="post" action="/details">
+
+<input required type="file" class="form-control" name="images[]" placeholder="address" multiple>
+
+
+public function store(request $request) {
+
+    $input=$request->all();
+    $images=array();
+    if($files=$request->file('images')){
+        foreach($files as $file){
+            $name=$file->getClientOriginalName();
+            $file->move('image',$name);
+            $images[]=$name;
+        }
+    }
+    /*Insert your data*/
+
+    Detail::insert( [
+        'images'=>  implode("|",$images),
+        'description' =>$input['description'],
+        //you can put other insertion here
+    ]);
+
+
+    return redirect('redirecting page');
+}
